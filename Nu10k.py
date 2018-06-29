bb135eac213c3ff1f358f716d447d46b3d390a62
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-30 01:42:15 +0700

    4

--- Nu10k.py
+++ Nu10k.py
@@ -1789,7 +1789,7 @@ def lineBot(op):
                         ki5.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki6.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki7.acceptGroupInvitationByTicket(op.param1,Ticket)
-			ki8.acceptGroupInvitationByTicket(op.param1,Ticket)		
+                        ki8.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki9.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki10.acceptGroupInvitationByTicket(op.param1,Ticket)
                         G = ki2.getGroup(op.param1)
