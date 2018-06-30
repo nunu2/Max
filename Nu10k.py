ca1659356c18fc2a515befb33572bc82ddd74419
sunu2018 <39335771+sunu2018@users.noreply.github.com>
2018-06-30 11:05:31 +0700

    ภ

--- Nu10k.py
+++ Nu10k.py
@@ -1862,7 +1862,7 @@ def lineBot(op):
                         ki7.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki8.acceptGroupInvitationByTicket(op.param1,Ticket)
                         ki9.acceptGroupInvitationByTicket(op.param1,Ticket)
-			ki10.acceptGroupInvitationByTicket(op.param1,Ticket)                        
+                        ki10.acceptGroupInvitationByTicket(op.param1,Ticket)
                         G = ki3.getGroup(op.param1)
                         G.preventedJoinByTicket = True
                         ki3.updateGroup(G)
